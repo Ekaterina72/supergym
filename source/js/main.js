038b6d0a19d956a3b9847972bb4bac42f0acf4f1
ekaterina <ekivanova72@gmail.com>
2023-04-25 23:01:22 +0500

    subs begining

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {Form} from './modules/form-validate/form';
 import {initVideo} from './modules/video/video';
+import {initTabs} from './modules/tabs/init-tabs';
 
 // ---------------------------------
 
@@ -21,6 +22,7 @@ window.addEventListener('DOMContentLoaded', () => {
     window.form = form;
     form.init();
     initVideo();
+    initTabs();
   });
 });
 
