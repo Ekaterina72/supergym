c4de60879ec4435f3e99ef5d5e9857e2f90afd3e
ekaterina <ekivanova72@gmail.com>
2023-04-29 20:12:01 +0500

    coaches slider

--- source/js/modules/sliders/swiper-coaches.js
+++ source/js/modules/sliders/swiper-coaches.js
@@ -1,9 +1,32 @@
 import Swiper from '../../vendor/swiper';
 
-const swiperCoaches = new Swiper('.swiper-coaches', {
+const swiperCoaches = new Swiper('.coaches__slider', {
   loop: true,
   slidesPerView: 4,
   spaceBetween: 40,
+  grabCursor: true,
+
+  breakpoints: {
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+  },
 
   navigation: {
     nextEl: '.swiper-button-next',
